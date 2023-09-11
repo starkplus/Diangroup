2472543f1607a0b5a4afaed1e54c96c195126493
starkplus <132072954+starkplus@users.noreply.github.com>
2023-09-11 23:03:57 +0800

    Update libwyag.py

---
+++ libwyag.py
@@ -0,0 +1,16 @@
+import argparse
+import collections
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
